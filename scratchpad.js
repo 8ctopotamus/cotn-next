c801d9e31e1a5d18016af181bb0cb7557f20c8be
Josh <zylo.codes@gmail.com>
2021-02-07 20:48:17 -0600

    Can read file content in folders

--- scratchpad.js
+++ scratchpad.js
@@ -7,22 +7,20 @@ const privToken = `private_token=${process.env.GITLAB_PERSONAL_ACCESS_TOKEN}`
 
 const fetchGitlabData = async () => {
   for (const dir of directories) {
-    const { data: tree } = await axios(`https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/tree?ref=${branch}&path=${dir}&${privToken}`)
-
-    const jsonFiles = tree
-      .filter(({ name }) => name.toLowerCase().includes('.json'))
-    
-    const contents = []
-    for (const obj of jsonFiles) {
-      const queryURL = `https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/files/${obj.path}/raw?ref=dev&${privToken}`
-
-      console.log(queryURL)
-
-      const { data: content } = await axios(queryURL)
-      contents.push(content)
+    try {
+      const { data: tree } = await axios(`https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/tree?ref=${branch}&path=${dir}&${privToken}`)
+      const jsonFiles = tree.filter(({ name }) => name.toLowerCase().includes('.json'))
+      const contents = []
+      for (const obj of jsonFiles) {
+        const filepath = obj.path.replace(/\//g, '%2F').replace('.', '%2E')
+        const queryURL = `https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/files/${filepath}/raw?ref=dev&${privToken}`
+        const { data: content } = await axios(queryURL)
+        contents.push(content)
+      }
+      console.log(contents)
+    } catch(err) {
+      throw err
     }
-
-    console.log(contents)
   }
 }
 
