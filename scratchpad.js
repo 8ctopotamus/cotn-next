e257a811a2080871bef6c72ff73d00164be36157
Josh <zylo.codes@gmail.com>
2021-02-21 00:05:02 -0600

    refactors scratchpad to search all directories in site-content in repo

--- scratchpad.js
+++ scratchpad.js
@@ -2,22 +2,23 @@ require('dotenv').config()
 const axios = require('axios')
 
 const branch = 'dev'
-const directories = ['pages']
+const directories = ['site-content']
 const privToken = `private_token=${process.env.GITLAB_PERSONAL_ACCESS_TOKEN}`
 
 const fetchGitlabData = async () => {
   for (const dir of directories) {
     try {
       const { data: tree } = await axios(`https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/tree?ref=${branch}&path=${dir}&${privToken}`)
-      const jsonFiles = tree.filter(({ name }) => name.toLowerCase().includes('.json'))
-      const contents = []
-      for (const obj of jsonFiles) {
-        const filepath = obj.path.replace(/\//g, '%2F').replace('.', '%2E')
-        const queryURL = `https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/files/${filepath}/raw?ref=dev&${privToken}`
-        const { data: content } = await axios(queryURL)
-        contents.push(content)
+      for (const { name, path } of tree) {
+        const { data: files } = await axios(`https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/tree?ref=${branch}&path=${path}&${privToken}`)
+        const jsonFiles = files.filter(({ name }) => name.toLowerCase().includes('.json'))
+        for (const obj of jsonFiles) {
+          const filepath = obj.path.replace(/\//g, '%2F').replace('.', '%2E')
+          const queryURL = `https://gitlab.codeofthenorth.com/api/v4/projects/118/repository/files/${filepath}/raw?ref=dev&${privToken}`
+          const { data: content } = await axios(queryURL)
+          console.log(name, content)
+        }
       }
-      console.log(contents)
     } catch(err) {
       throw err
     }
