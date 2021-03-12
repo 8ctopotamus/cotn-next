77151b0eba979aa21906d56f7dc750d616e3e501
Josh <zylo.codes@gmail.com>
2021-03-11 23:11:17 -0600

    starts careers archive

---
+++ pages/careers/index.js
@@ -0,0 +1,11 @@
+import Head from 'next/head';
+import Layout from '../../components/layout';
+const CareersIndex = () => {
+  return (
+    <Layout>
+      Test
+    </Layout>
+  );
+};
+
+export default CareersIndex;
